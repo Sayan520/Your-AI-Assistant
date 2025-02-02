631e98a889adf1ac2df89a32d9ac44f61390d442
Sayan Ghosh <ghoshsayan5205@gmail.com>
2025-02-02 11:45:34 +0530

    Added .gitignore file

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Aiinhome POCs\Python\GEMINI\Your-Friendly-Gemini-Chatbot-\venv
+set VIRTUAL_ENV=C:\Github Projects\Your-AI-Assistant\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
